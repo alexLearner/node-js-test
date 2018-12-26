a4754d2f3dbd24fd932f32cf5148d4fb75cd457a
Alex Maltsev <alexq.dev@gmail.com>
2018-12-26 02:58:45 +0200

    remove comments

--- config/passport.js
+++ config/passport.js
@@ -13,9 +13,6 @@ passport.deserializeUser((id, done) => {
   });
 });
 
-/**
- * Sign in using Email and Password.
- */
 passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
   User.findOne({ email: email.toLowerCase() }, (err, user) => {
     if (err) { return done(err); }
